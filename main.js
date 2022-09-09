713cc5e9f9cd8284eaa60f366585dc41cc3fb170
vidhya <vidhya.raghav21@gmail.com>
2022-09-09 15:53:42 +0530

    modified the files for mobile responsiveness

---
+++ main.js
@@ -0,0 +1,11 @@
+function menuClick()
+{
+    if (document.querySelector('.menudiv').style.display==="")
+    {
+        document.querySelector('.menudiv').style.display="block";
+    }
+    else
+    {
+        document.querySelector('.menudiv').style.display="";
+    }
+}
